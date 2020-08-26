34147ba2cfd739a1b7df19f1eebeb7a1f509d94c
Rodion Golovushkin <rodion.golovushkin@atnr.pro>
2020-08-26 20:22:59 +0700

    Add firebase deploy support

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCEsF8mwskqeubMUJfJeLXLIory-AlG0Ks',
+  authDomain: 'easyreader-56a60.firebaseapp.com',
+  databaseURL: 'https://easyreader-56a60.firebaseio.com',
+  projectId: 'easyreader-56a60',
+  storageBucket: 'easyreader-56a60.appspot.com',
+  messagingSenderId: '792027254518',
+  appId: '1:792027254518:web:2be6ccabb02f8b3223a6d7',
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
