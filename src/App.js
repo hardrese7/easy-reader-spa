f526e50e660de576c4970443695dec76ffed2ec5
Rodion Golovushkin <rodion.golovushkin@atnr.pro>
2020-08-26 20:39:22 +0700

    Implement google auth

--- src/App.js
+++ src/App.js
@@ -1,13 +1,57 @@
-import React from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import { Button } from '@material-ui/core';
+import * as firebase from 'firebase/app';
 import cn from './App.module.scss';
 import 'src/styles/global.scss';
 
 function App() {
+  const [token, setToken] = useState(null);
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState(null);
+  const provider = useMemo(() => new firebase.auth.GoogleAuthProvider(), []);
+  const login = useCallback(() => {
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then((result) => {
+        setToken(result.credential.accessToken);
+        setUser(result.user);
+      })
+      .catch((error) => {
+        setError(error);
+      });
+  }, [provider]);
+  const logout = useCallback(() => {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        setToken(null);
+        setUser(null);
+      })
+      .catch((error) => {
+        setError(error);
+      });
+  }, []);
   return (
-    <Button className={cn.Button} color="primary">
-      Hello World
-    </Button>
+    <div>
+      <Button onClick={login} className={cn.Button} color="primary">
+        Login
+      </Button>
+      <Button onClick={logout} className={cn.Button} color="primary">
+        Logout
+      </Button>
+      <br />
+      Token:
+      {JSON.stringify(token)}
+      <br />
+      User:
+      {JSON.stringify(user)}
+      <br />
+      Error:
+      {JSON.stringify(error)}
+      <br />
+    </div>
   );
 }
 
