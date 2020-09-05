fba485e078c0be0752e5d9bcfda5cb55ca86c2db
Rodion Golovushkin <rodion.golovushkin@atnr.pro>
2020-09-05 18:32:13 +0700

    Implement texts page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 import PrivateRoute from './PrivateRoute';
 import HomePage from './pages/Home';
 import AddTextPage from './pages/AddText';
+import TextsPage from './pages/Texts';
 import AdminPage from './pages/Admin';
 import LoginPage from './pages/Login';
 import TopBar from './common/TopBar';
@@ -26,10 +27,14 @@ function App() {
             <li>
               <Link to="/add-text">Add text</Link>
             </li>
+            <li>
+              <Link to="/texts">Texts</Link>
+            </li>
           </ul>
           <Route exact path="/" component={HomePage} />
           <Route path="/login" component={LoginPage} />
           <PrivateRoute path="/add-text" component={AddTextPage} />
+          <PrivateRoute path="/texts" component={TextsPage} />
           <PrivateRoute path="/admin" component={AdminPage} />
         </div>
       </Router>
---
+++ src/pages/Texts.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react';
+import * as firebase from 'firebase/app';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+import { useAuth } from 'src/context/auth';
+
+function Texts() {
+  const { currentUser } = useAuth();
+  const [texts, setTexts] = useState([]);
+  useEffect(() => {
+    const textsRef = firebase.database().ref(`texts/${currentUser.uid}`);
+    textsRef.on('value', (snapshot) => {
+      const textsArr = [];
+      snapshot.forEach((s) => {
+        textsArr.push({ ...s.val(), id: s.key });
+      });
+      setTexts(textsArr);
+    });
+    return () => textsRef.off('value');
+  }, [currentUser]);
+  return (
+    <List component="nav">
+      {texts.map((t) => (
+        <ListItem key={t.id} button>
+          <ListItemText primary={t.title} secondary={'Progress 0%'} />
+          <ListItemSecondaryAction>
+            <IconButton edge="end" aria-label="delete">
+              <DeleteIcon />
+            </IconButton>
+          </ListItemSecondaryAction>
+        </ListItem>
+      ))}
+    </List>
+  );
+}
+
+export default Texts;
